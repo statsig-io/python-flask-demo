bd64768ff8fcc226369a39916e3cbb3472a3b089
Daniel Loomb <daniel@statsig.com>
2023-07-06 22:15:08 -0700

    update to StandaloneApplication

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
-import os
 from random import randint
-from statsig import statsig, StatsigUser
 from flask import Flask
+from statsig import statsig, StatsigUser
 
-statsig.initialize("secret-I9RZAyjyxdffBKd8B5Q9N9lYsTw0e1eo91uYqlOhmSU")
+import os
+import gunicorn.app.base
 
-app = Flask(__name__)
 
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
-@app.route('/')
-def root():
-    user = StatsigUser(user_id=f"user-{randint(1, 1000)}")
-    config = statsig.get_config(user, 'a_config')
-    return {"version": config.get('version'), "pid": os.getpid()}
+    def init(self, parser, opts, args):
+        pass
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        statsig.initialize("secret-I9RZAyjyxdffBKd8B5Q9N9lYsTw0e1eo91uYqlOhmSU")
+        return self.application
 
 
 if __name__ == '__main__':
-    app.run()
+    app = Flask(__name__)
+
+
+    @app.route('/')
+    def root():
+        user = StatsigUser(user_id=f"user-{randint(1, 1000)}")
+        config = statsig.get_config(user, 'a_config')
+        return {"version": config.get('version'), "pid": os.getpid()}
+
+
+    options = {
+        'bind': '127.0.0.1:8000',
+        'workers': 4,
+    }
+
+    StandaloneApplication(app, options).run()
